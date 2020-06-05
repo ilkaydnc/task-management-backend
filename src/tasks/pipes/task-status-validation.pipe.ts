543d8410cfe2ef52035df0a61f0c6c0b27ba35ae
Ilkay <social@ilkaydnc.dev>
2020-06-05 20:52:21 +0300

    Create validation pipe and Add validation to filter

---
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -0,0 +1,24 @@
+import { PipeTransform, BadRequestException } from '@nestjs/common';
+import { TaskStatus } from '../task.model';
+
+export class TaskStatusValidationPipe implements PipeTransform {
+  readonly alloweStatuses = [
+    TaskStatus.OPEN,
+    TaskStatus.IN_PROGRESS,
+    TaskStatus.DONE,
+  ];
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  transform(value: any): any {
+    console.log('value', value);
+
+    if (!this.isStatusValid(value))
+      throw new BadRequestException(`${value} is an invalid status!`);
+
+    return value;
+  }
+
+  private isStatusValid(status: any) {
+    return this.alloweStatuses.indexOf(status) !== -1;
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -14,13 +14,14 @@ import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
     if (Object.keys(filterDto).length)
       return this.tasksService.getTasksWithFilters(filterDto);
     else return this.tasksService.getAllTasks();
@@ -45,7 +46,7 @@ export class TasksController {
   @Patch('/:id/status')
   updateTaskStatus(
     @Param('id') id: string,
-    @Body('status') status: TaskStatus,
+    @Body('status', TaskStatusValidationPipe) status: TaskStatus,
   ): Task {
     return this.tasksService.updateTaskStatus(id, status);
   }
