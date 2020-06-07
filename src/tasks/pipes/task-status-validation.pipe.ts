4c4321653c58c966c961ca1db3c5db49f6227a25
Ilkay Ubuntu <ilkaydinc1@gmail.com>
2020-06-07 20:14:32 +0300

    Remove console log

--- src/tasks/pipes/task-status-validation.pipe.ts
+++ src/tasks/pipes/task-status-validation.pipe.ts
@@ -10,8 +10,6 @@ export class TaskStatusValidationPipe implements PipeTransform {
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   transform(value: any): any {
-    console.log('value', value);
-
     if (!this.isStatusValid(value))
       throw new BadRequestException(`${value} is an invalid status!`);
 
