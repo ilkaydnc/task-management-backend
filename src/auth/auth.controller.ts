673eb28cda6bb670fee3fa914a7ceaae0059246f
Ilkay Ubuntu <ilkaydinc1@gmail.com>
2020-06-07 14:39:31 +0300

    Add strength password regex

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -9,7 +9,7 @@ export class AuthController {
   ) { }
 
   @Post('/signup')
-  signUp(@Body() authCredentials: AuthCredentialsDto): Promise<void> {
+  signUp(@Body(ValidationPipe) authCredentials: AuthCredentialsDto): Promise<void> {
     return this.authService.signUp(authCredentials)
   }
 }
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,22 @@
+import { IsString, MinLength, MaxLength, Matches } from "class-validator";
+
+const checkLower = '(?=.*[a-z])'
+const checkUpper = '(?=.*[A-Z])'
+const checkNumber = '(?=.*\d)'
+const checkSymbol = '(?=.*?[#?!@$%^&*-])'
+
+const passwordRegex = new RegExp(`${checkLower}${checkUpper}${checkNumber}${checkSymbol}`, "g")
+
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  @Matches(passwordRegex, { message: "password is too weak. " })
   password: string;
 }
