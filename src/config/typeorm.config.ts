38776a8beed952ac02b27b554cb3cd0f0678898b
Ilkay Ubuntu <ilkaydinc1@gmail.com>
2020-06-07 11:57:17 +0300

    Refactor task module with database connection

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -7,6 +7,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: "postgres",
   password: "postgres",
   database: "postgres",
-  entities: [__dirname + '/../**/*.entity.ts'],
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,64 +1,83 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Task, TaskStatus } from './task.model';
-import { v1 as uuid } from 'uuid';
+import { InjectRepository } from '@nestjs/typeorm';
+import { TaskRepository } from './task.repository';
+import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatus } from './task-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
-  private tasks: Task[] = [];
+  constructor(
+    @InjectRepository(TaskRepository)
+    private taskRepository: TaskRepository
+  ) {
 
-  getAllTasks(): Task[] {
-    return this.tasks;
   }
 
-  getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-    const { status, search } = filterDto;
-    let tasks = this.tasks;
-
-    if (status) tasks = tasks.filter(task => task.status == status);
-
-    if (search)
-      tasks = tasks.filter(
-        task =>
-          task.title.toLowerCase().includes(search.toLowerCase()) ||
-          task.description.toLowerCase().includes(search.toLowerCase()),
-      );
-
-    return tasks;
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    return this.taskRepository.getTasks(filterDto)
   }
 
-  getTaskById(id: string): Task {
-    const task = this.tasks.find(task => task.id == id);
+  async getTaskById(id: number): Promise<Task> {
+    const task = await this.taskRepository.findOne(id)
 
     if (!task) throw new NotFoundException(`Task with ID ${id} not found!`);
 
-    return task;
+    return task
   }
 
-  createTask(createTaskDto: CreateTaskDto): Task {
-    const { title, description } = createTaskDto;
-    const task: Task = {
-      id: uuid(),
-      title,
-      description,
-      status: TaskStatus.OPEN,
-    };
-
-    this.tasks.push(task);
-
-    return task;
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskRepository.createTask(createTaskDto)
   }
 
-  deleteTask(id: string): void {
-    const found = this.getTaskById(id);
-    this.tasks = this.tasks.filter(task => task != found);
+  async deleteTask(id: number): Promise<void> {
+    const response = await this.taskRepository.delete(id)
+
+    if (!response.affected) throw new NotFoundException(`Task with ID ${id} not found!`);
   }
 
-  updateTaskStatus(id: string, status: TaskStatus): Task {
-    const task = this.getTaskById(id);
-    task.status = status;
+  async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
+    const task = await this.getTaskById(id)
+    task.status = status
 
-    return task;
+    await task.save()
+
+    return task
   }
+
+
+  // getTaskById(id: string): Task {
+  //   const task = this.tasks.find(task => task.id == id);
+
+  //   if (!task) throw new NotFoundException(`Task with ID ${id} not found!`);
+
+  //   return task;
+  // }
+
+  // createTask(createTaskDto: CreateTaskDto): Task {
+  //   const { title, description } = createTaskDto;
+  //   const task: Task = {
+  //     id: uuid(),
+  //     title,
+  //     description,
+  //     status: TaskStatus.OPEN,
+  //   };
+
+  //   this.tasks.push(task);
+
+  //   return task;
+  // }
+
+  // deleteTask(id: string): void {
+  //   const found = this.getTaskById(id);
+  //   this.tasks = this.tasks.filter(task => task != found);
+  // }
+
+  // updateTaskStatus(id: string, status: TaskStatus): Task {
+  //   const task = this.getTaskById(id);
+  //   task.status = status;
+
+  //   return task;
+  // }
 }
