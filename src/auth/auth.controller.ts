073d3ce49e0bdbc842a06c9c2c85124c3f391202
Ilkay Ubuntu <ilkaydinc1@gmail.com>
2020-06-07 12:36:14 +0300

    Create Auth module and Add very simple signup method

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  @Post('/signup')
+  signUp(@Body() authCredentials: AuthCredentialsDto): Promise<void> {
+    return this.authService.signUp(authCredentials)
+  }
+}
---
+++ src/auth/user.entity.ts
@@ -0,0 +1,13 @@
+import { BaseEntity, PrimaryGeneratedColumn, Column, Entity } from "typeorm";
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  username: string
+
+  @Column()
+  password: string
+}
