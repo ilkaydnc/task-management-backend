ea5dc0470ea19730652b33bd13baa6ac9d641c44
Ilkay Ubuntu <ilkaydinc1@gmail.com>
2020-06-08 12:55:31 +0300

    Create jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common"
+import { InjectRepository } from "@nestjs/typeorm"
+import { PassportStrategy } from "@nestjs/passport"
+import { Strategy, ExtractJwt } from "passport-jwt"
+import { JwtPayload } from "./jwt-payload.interface"
+import { UserRepository } from "./user.repository"
+import { User } from "./user.entity"
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: "test_secret_42"
+    })
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload
+    const user = await this.userRepository.findOne({ username })
+
+    if (!user) throw new UnauthorizedException("test")
+
+    return user
+  }
+
+}
