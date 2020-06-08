391edb23b6fc750eba870c1147d781d3a2664bd2
Ilkay Ubuntu <ilkaydinc1@gmail.com>
2020-06-08 12:57:23 +0300

    Add jwt service to auth service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,7 +14,7 @@ export class AuthController {
   }
 
   @Post("/signin")
-  signIn(@Body(ValidationPipe) authCredentials: AuthCredentialsDto): Promise<void> {
+  signIn(@Body(ValidationPipe) authCredentials: AuthCredentialsDto): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentials)
   }
 }
